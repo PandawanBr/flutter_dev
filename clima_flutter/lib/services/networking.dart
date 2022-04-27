7cc43b0fb242a458056012d475b703db24e00ec0
Rodrigo Kobayashi <rodrigo.k.barboza@accenture.com>
2022-04-26 22:48:43 -0300

    Clima App

---
+++ clima_flutter/lib/services/networking.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  NetworkHelper(this.uri);
+
+  final Uri uri;
+
+  Future getData() async {
+    http.Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
